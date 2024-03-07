040c860377de11aef0e6640856ef4bd79b556919
AmirAli Seyedzadegan <mahbodseyedzadegan@gmail.com>
2024-03-08 01:01:16 +0330

    Update task2.py

--- answers/YourStudentNumber/40030212040/task2.py
+++ answers/YourStudentNumber/40030212040/task2.py
@@ -8,6 +8,8 @@ npList = np.array(pList)
 # accessing the third element:
 thirdElement = npList[2]
 
+print("Third element of the One-dimensional array: ")
+print(thirdElement)
 # -----------------------------------------------
 
 # Two-dimensional matrix:
@@ -16,12 +18,30 @@ threeByFourMatrix = np.zeros((3, 4))
 # accessing the [2, 3] element:
 element2And3 = threeByFourMatrix[2, 3]
 
+print("\n[2,3] element of the Two-dimensional array: ")
+print(element2And3)
+print("\n/-----------------\\")
 # /-----------------Slicing-----------------\
 
 # General syntax for slicing in NumPy: {array[start:stop:step]}
 # Extracting specific rows or columns from a 2D array: 
 #           array[start_row:end_row, start_col:end_col] 
 subMatrix = threeByFourMatrix[:2, :2]
+print("\nSliced sub-matrix: \n")
+print(subMatrix)
+print("\n/-----------------\\")
+print()
 
-# -----------------------------------------------
+# /-----------------Iterating-----------------\
+
+# Three-dimensional matrix:
 tDM =  np.ones((2,3,4))
+
+# Iterating over the first dimension :
+print("Iteration: ")
+for i in range(tDM.shape[0]):
+    # Printing each two-dimensional section
+    print(f"{i+1}. ")
+    print(tDM[i])
+    print()
+    
