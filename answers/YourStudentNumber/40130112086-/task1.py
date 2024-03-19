e78f804fcc6d4d043b9b7301b9e0d747865156f0
Tina-Farshadi <tina.fsh83@gmail.com>
2024-03-19 10:54:46 +0330

    creating 3*4 matrix

--- answers/YourStudentNumber/40130112086-/task1.py
+++ answers/YourStudentNumber/40130112086-/task1.py
@@ -1,6 +1,12 @@
 import numpy as np
-
+#task1.1
 #Creating Python list
 task1_list = [2,4,6,8]
 #Creating NumPy array
 task1_array = np.array(task1_list)
+print(task1_list)
+
+#task1.2
+#creating 3*4 matrix
+task1_matrix=np.zeros((3,4))
+print(task1_matrix)
