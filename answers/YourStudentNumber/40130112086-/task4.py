c4a073728d4c49d11ad93b7502adf00f7d2c3db8
Tina-Farshadi <tina.fsh83@gmail.com>
2024-03-19 13:49:49 +0330

    Matrix Multiplication of task4

---
+++ answers/YourStudentNumber/40130112086-/task4.py
@@ -0,0 +1,9 @@
+import numpy as np
+#Matrix Multiplication
+#creaating matrix
+first=np.array([[3, 4], [5, 6]])
+second=np.array([[1, 2], [4, 8]])
+#dot product
+multiplication= np.dot(first,second)
+print(multiplication)
+
