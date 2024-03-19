1d9da16841987232d6de50a0125ececaab1b73b4
Tina-Farshadi <tina.fsh83@gmail.com>
2024-03-19 13:53:04 +0330

    Determinant and Inverse of task 4

--- answers/YourStudentNumber/40130112086-/task4.py
+++ answers/YourStudentNumber/40130112086-/task4.py
@@ -7,3 +7,13 @@ second=np.array([[1, 2], [4, 8]])
 multiplication= np.dot(first,second)
 print(multiplication)
 
+#Determinant and Inverse
+#creating matrix
+matrix= np.array([[1, 2], [3, 4]])
+#the determinant of the matrix
+determinant = np.linalg.det(matrix)
+# the inverse of the matrix
+inverse = np.linalg.inv(matrix)
+print("\nDeterminant of the matrix:",determinant)
+print("\nInverse of the matrix:",inverse)
+
