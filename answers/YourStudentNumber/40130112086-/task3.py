84b3d72ee14cf641099e9f2bdd97825c746d0919
Tina-Farshadi <tina.fsh83@gmail.com>
2024-03-19 13:24:06 +0330

    Broadcasting of task3 is done

--- answers/YourStudentNumber/40130112086-/task3.py
+++ answers/YourStudentNumber/40130112086-/task3.py
@@ -1,7 +1,9 @@
 import numpy as np
 #Arithmetic Operations
+#creating arrays
 First_array = np.array([[4, 2], [3, 9]])
 Second_array = np.array([[2, 1], [8, 3]])
+#operations
 addition=First_array+Second_array
 subtraction=First_array-Second_array
 multiplication=First_array*Second_array
@@ -10,3 +12,12 @@ print("addition",addition)
 print("Subtraction",subtraction)
 print("multiplication",multiplication)
 print("division",division)
+
+#Broadcasting:
+#Demonstrating array broadcasting
+#creating matrix and vector
+matrix = np.array([[9, 10, 31], [0, 6, 55]])
+vector = np.array([3,6])
+broadcasting = matrix + vector[:, np.newaxis]
+print("\n",broadcasting)
+
