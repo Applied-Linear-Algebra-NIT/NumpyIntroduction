c8dd4e54161ed0c8a655db157fcea45cf26d2ffd
Tina-Farshadi <tina.fsh83@gmail.com>
2024-03-19 13:13:30 +0330

    Arithmetic Operations of task3 is done

---
+++ answers/YourStudentNumber/40130112086-/task3.py
@@ -0,0 +1,12 @@
+import numpy as np
+#Arithmetic Operations
+First_array = np.array([[4, 2], [3, 9]])
+Second_array = np.array([[2, 1], [8, 3]])
+addition=First_array+Second_array
+subtraction=First_array-Second_array
+multiplication=First_array*Second_array
+division=First_array/Second_array
+print("addition",addition)
+print("Subtraction",subtraction)
+print("multiplication",multiplication)
+print("division",division)
