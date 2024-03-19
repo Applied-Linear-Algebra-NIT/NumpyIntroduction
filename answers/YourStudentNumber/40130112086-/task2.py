07a7524b19d7755bd12980e4f0a4e4c24a2a667c
Tina-Farshadi <tina.fsh83@gmail.com>
2024-03-19 11:51:31 +0330

    Indexing of task2 is done

---
+++ answers/YourStudentNumber/40130112086-/task2.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#indexing
+#Accessing the third element
+array=[1,2,3]
+third_element=array[2]
+
+#Accessing the element at position [2, 3]
+matrix = np.array([[2,4,6], [8,10,12]])
+elemet=matrix[1,2]
